f3d38484599ed94fad91bd8226ac853baf827fff
Darius Fang <dariusfang1@gmail.com>
2024-12-14 16:16:40 -0700

    Initial commit

---
+++ Source/UE4Fundamentals/UE4FundamentalsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4FundamentalsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4FUNDAMENTALS_API AUE4FundamentalsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
